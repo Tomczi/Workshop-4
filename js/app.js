5bee406a118ed3ea584e937f58c20103b19b0852
Tomczi <sebastian.tomczuk5@gmail.com>
2018-05-27 15:15:51 +0200

    Ex3

---
+++ js/app.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    
+    var rootDiv = $("#root");
+
+    $.ajax({
+        url: "http://localhost:8282/books/",
+        type: "GET"
+    }).done(function(data) {
+
+
+        for (var i = 0; i < data.length; i++) {
+            var bookElement = $("<div>" + data[i].title +"<div></div></div>")
+            rootDiv.append(bookElement);
+
+        }
+        
+    })
+
+})
